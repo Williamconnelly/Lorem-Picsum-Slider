8f12847f8eef8c1f5e609502242d7be3f83d888f
William Connelly <williamconnelly.dev@gmail.com>
2019-01-17 01:05:41 -0800

    Added automated slide toggle and placeholder slide animations

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -18,6 +18,8 @@ interface Slide {
 export class SliderComponent implements OnInit {
   slides: Array<Object> = [];
   currentIndex = 0;
+  // Upper-scope variable so that different methods can start and stop it
+  interval = null;
   constructor(private _slideService: SlideService) { }
 
   ngOnInit() {
@@ -31,26 +33,51 @@ export class SliderComponent implements OnInit {
       });
     });
   }
+  // Advance slideshow forward
   nextSlide() {
+    // Grab all slide elements
     const referenceSlides = document.getElementsByClassName('slide');
+    // If currently on the final slide, reset the index
     if (this.currentIndex === this.slides.length - 1) {
       referenceSlides[this.currentIndex].style.display = 'none';
       this.currentIndex = 0;
+      // else, increase the index by 1
     } else {
       this.currentIndex += 1;
       referenceSlides[this.currentIndex - 1].style.display = 'none';
     }
+    // Set the slide of the new current index to visible
+    referenceSlides[this.currentIndex].classList.remove('slide-left');
+    referenceSlides[this.currentIndex].classList.add('slide-right');
     referenceSlides[this.currentIndex].style.display = 'block';
   }
+  // Revert slideshow back
   previousSlide() {
+    // Grab all slide elements
     const referenceSlides = document.getElementsByClassName('slide');
+    // If currently on the first slide, move index to the last slide
     if (this.currentIndex === 0) {
       referenceSlides[this.currentIndex].style.display = 'none';
       this.currentIndex = this.slides.length -1;
+      // else, reduce the index by 1
     } else {
       this.currentIndex -= 1;
       referenceSlides[this.currentIndex + 1].style.display = 'none';
     }
+    // Set the slide of the new current index to visible
+    referenceSlides[this.currentIndex].classList.remove('slide-right');
+    referenceSlides[this.currentIndex].classList.add('slide-left');
     referenceSlides[this.currentIndex].style.display = 'block';
+  }
+  // Sets or clears an interval that calls nextSlide()
+  toggleAutomate() {
+    if (this.interval !== null) {
+      clearInterval(this.interval);
+      this.interval = null;
+    } else {
+      this.interval = setInterval(() => {
+        this.nextSlide();
+      }, 2000);
+    }
   }
 }
